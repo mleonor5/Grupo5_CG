82c6996d8f8db9c9561b31600bb5587f6f69614f
tomasnborges <tomas.borges.7007@gmail.com>
2021-11-23 13:01:48 +0000

    askdkasdk

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 const ctx = canvas.getContext('2d');
 
-const W = canvas.width
-const H = canvas.height
+let W = canvas.width
+let H = canvas.height, H1, W1
 //balls (array of objects)
 let b = new Array()
 let balas = new Array()
+let resize = false
+const aToB = Math.sqrt(H**2+W**2)/1
 
 
 //As variáveis vão contar a distância que o triângulo tem que mover em resultado da tecla precionada
@@ -30,8 +32,7 @@ const debounce = function (func) {    // função debouncing inspirada do site h
 
 
 window.addEventListener('resize', debounce((function () {
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
+    resize = true
 })))
 
 //evento para quando a tecla é precionada
@@ -51,8 +52,6 @@ window.addEventListener('keydown', e => {
             shot = true
         }
     }  
-
-    e.preventDefault();
 
 });
 
@@ -88,9 +87,9 @@ class Bullet{
         this.y += this.yd * 2
         this.x += this.xd * 2
 
-        /* if(this.y <= 0){
-            balas.shift()
-        } */
+        if(this.R + this.x < 0 || this.R + this.x > W|| this.R + this.y > H|| this.R + this.y < 0){
+            this.destroyed = true
+        }
     }
 }
 
@@ -139,19 +138,21 @@ class Ball {
     }
 
     leftCanvas() {
-        if (this.y >= H) {
-            this.color = 'white'
-            this.y = -25
+        if (this.y >= H+this.R) {    //Se o circulo ultrapassou a borda inferior
+            this.y = -this.R
             this.x = 20 + Math.random() * (W - 2 * 20)
-            // this.v = 0.75 + Math.random() * 5
-        } else if (this.x >= W) {
-            this.color = 'white'
-            this.x = -25
+        } else if (this.y <= 0-this.R) {    //Se o circulo ultrapassou a borda superior
+            this.y = H+this.R
+            this.x = 20 + Math.random() * (W - 2 * 20)
+        }else if (this.x >= W+this.R) {    //Se o circulo ultrapassou a borda direita
+            this.x = -this.R
             this.y = 20 + Math.random() * (W - 2 * 20)
-            // this.v = 0.75 + Math.random() * 5
-        }
-
+        } else if (this.x <= 0-this.R) {    //Se o circulo ultrapassou a borda esquerda
+            this.x = W+this.R
+            this.y = 20 + Math.random() * (W - 2 * 20)
+        } 
     }
+
 }
 
 let rightAngle = Math.PI * 60 / 180   // ângulo do ponto direito
@@ -161,12 +162,12 @@ let upPointX, upPointY
 
 //função que desenha o triângulo
 function drawTriangle(){
-    upPointX = deltaX + 15 * Math.cos(upAngle)  // Coordenada X do ponto de onde saiem as balas
-    upPointY = deltaY + 15 * Math.sin(upAngle)  // Coordenada Y do ponto de onde saiem as balas
-    let leftPointX = deltaX + 15 * Math.cos(leftAngle) // Coordenada X do ponto esquerdo
-    let leftPointY = deltaY + 15* Math.sin(leftAngle) // Coordenada Y do ponto esquerdo
-    let rightPointX = deltaX + 15 * Math.cos(rightAngle) // Coordenada X do ponto direito
-    let rightPointY = deltaY + 15* Math.sin(rightAngle) // Coordenada Y do ponto direito
+    upPointX = deltaX + 35 * Math.cos(upAngle)  // Coordenada X do ponto de onde saiem as balas
+    upPointY = deltaY + 35 * Math.sin(upAngle)  // Coordenada Y do ponto de onde saiem as balas
+    let leftPointX = deltaX + 35 * Math.cos(leftAngle) // Coordenada X do ponto esquerdo
+    let leftPointY = deltaY + 35* Math.sin(leftAngle) // Coordenada Y do ponto esquerdo
+    let rightPointX = deltaX + 35 * Math.cos(rightAngle) // Coordenada X do ponto direito
+    let rightPointY = deltaY + 35* Math.sin(rightAngle) // Coordenada Y do ponto direito
     
     ctx.fillStyle = "white"
     ctx.beginPath();
@@ -200,46 +201,117 @@ function init() {
     }
 }
 
+//Função que desenha a nave inimiga no canvas
+function enemy(){
+    //imagem
+    let img = new Image()
+    img.src = './enemy1.png'
+    //coordenadas iniciais(para já está estas coordendas até conseguir fazer o seu movimento)
+    let imgX = 300
+    let imgY = 200
+    //desenha a imagem no canvas
+    ctx.drawImage(img, imgX, imgY, img.width/2.6, img.height/2.6)
+}
+
+//Função que mostra o número de pontos(Falta a parte que conta os pontos que o utilizador ganha)
+function playerPoints(){
+    var text = 'Pontos:'
+    let points = 0
+    ctx.font = '20px Verdana'
+    ctx.fillStyle = 'White'
+    ctx.fillText(text, 50, 70)
+    ctx.fillText(points, 140, 72) 
+}
+
+//Função que mostra o número de vidas que o jogador tem(Falta a parte de diminuir uma vida quando o jogador é atingido por asteroide ou nave inimiga)
+function playerLives(){
+    var text = 'Lives:'
+    let lives = 3
+    ctx.font = '20px Verdana'
+    ctx.fillStyle = 'White'
+    ctx.fillText(text, 50, 100)
+    ctx.fillText(lives, 140, 102)
+}
+
 //function render
 function render() {
-    //fade Canvas
-    ctx.fillStyle = "black"
-    ctx.fillRect(0, 0, W, H);
-    if(rightKey){
-         rightAngle +=0.05
-         leftAngle +=0.05
-         upAngle +=0.05
-    };
-    if(leftKey){
-        leftAngle -=0.05
-        rightAngle -=0.05
-        upAngle -=0.05
+    if(resize){
+        H1 = H
+        W1 = W
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        H = window.innerHeight;      
+        W = window.innerWidth;
+        b.forEach(function (ball) {
+            //GRAÇAS AOS TESTES 1 E 2
+            let xB = ((ball.x + ball.dx)* W) / W1 
+            let xA = ball.x = (ball.x * W) / W1
+            let yB = ((ball.y + ball.dy)* H) / H1 
+            let yA = ball.y = (ball.y * H) / H1
+            let xVetorAB = xB - xA 
+            let yVetorAB = yB - yA 
+            let xVetorAC = W - xA 
+            let denominador = xVetorAB * xVetorAC 
+            let normaAB = Math.sqrt(xVetorAB**2 + yVetorAB**2)
+            let normaAC = Math.sqrt(xVetorAC**2)
+            let numerador = Math.abs(normaAB * normaAC)
+            let cos = denominador / numerador
+            let angulo = Math.acos(cos)
+            //GRAÇAS AO TESTE 3
+            ball.d < 0 ? angulo = -angulo : "" 
+            ball.dx = ball.v * Math.cos(angulo)
+            ball.dy = ball.v * Math.sin(angulo)
+
+        })
+
+        resize = false
+    }else{
+        //fade Canvas
+        ctx.fillStyle = "black"
+        ctx.fillRect(0, 0, W, H);
+        if(rightKey){
+            rightAngle +=0.05
+            leftAngle +=0.05
+            upAngle +=0.05
+        };
+        if(leftKey){
+            leftAngle -=0.05
+            rightAngle -=0.05
+            upAngle -=0.05
+        }
+        if(shot){
+            let color = `white`;
+            let xInit = upPointX
+            let yInit = upPointY
+            let radius = 2
+            let yDirection = Math.sin(upAngle)
+            let xDirection = Math.cos(upAngle)
+            balas.push(new Bullet(xInit, yInit, radius, color, yDirection, xDirection)) 
+            shot = false
+        }
+        if(upKey){
+            deltaY += Math.sin(upAngle)
+            deltaX += Math.cos(upAngle)
+        }
+        //draw update
+        b.forEach(function (ball) {
+            ball.draw()
+            ball.update()
+            ball.leftCanvas()
+        })
+
+        //desenhar e atualizar as balas 
+        for(let i = 0; i < balas.length; i++){
+            balas[i].draw()
+            balas[i].update()
+            if(balas[i].destroyed){
+                balas.splice(i, 1)
+            }
+        }
+        drawTriangle()
+        enemy()
+        playerPoints()
+        playerLives()
     }
-    if(shot){
-        let color = `white`;
-        let xInit = upPointX
-        let yInit = upPointY
-        let radius = 2
-        let yDirection = Math.sin(upAngle)
-        let xDirection = Math.cos(upAngle)
-        balas.push(new Bullet(xInit, yInit, radius, color, yDirection, xDirection)) 
-        shot = false
-    }
-    if(upKey){
-        deltaY += Math.sin(upAngle)
-        deltaX += Math.cos(upAngle)
-    }
-    //draw update
-    b.forEach(function (ball) {
-        ball.draw()
-        ball.update()
-        ball.leftCanvas()
-    })
-    balas.forEach(bullet =>{
-        bullet.draw();
-        bullet.update();
-        
-    })
-    drawTriangle()
     window.requestAnimationFrame(render)
 }
